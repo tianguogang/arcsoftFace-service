5cca0503296cc116518db1b0548683e1c84686e4
tianguogang <1369729295@qq.com>
2023-04-16 20:07:57 +0800

    项目初始化

---
+++ src/main/java/com/tgg/arcsoftfaceservice/ArcsoftFaceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tgg.arcsoftfaceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcsoftFaceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArcsoftFaceServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tgg/arcsoftfaceservice/ArcsoftFaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgg.arcsoftfaceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcsoftFaceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
